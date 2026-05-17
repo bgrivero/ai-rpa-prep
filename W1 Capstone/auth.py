4db0f16c0de8ad86980e067131a6c4bec88f0ce8
eevicteen <brycerivero@gmail.com>
2026-05-18 07:54:16 +0800

    Replaced Tutorial secretkey/algorithm with protected values from env file

--- W1 Capstone/auth.py
+++ W1 Capstone/auth.py
@@ -8,10 +8,13 @@ from jwt.exceptions import InvalidTokenError
 from pwdlib import PasswordHash
 from pydantic import BaseModel
 
+import os
+from dotenv import load_dotenv
 # to get a string like this run:
 # openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 fake_users_db = {
